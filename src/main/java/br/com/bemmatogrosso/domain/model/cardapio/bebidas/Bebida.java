377cd2c1329c06092a693ce9b257db92994a3139
Mailon Bruno <113206774+mailoncampos@users.noreply.github.com>
2023-02-05 19:25:20 -0400

    refactor: package controller

--- src/main/java/br/com/bemmatogrosso/domain/model/cardapio/bebidas/Bebida.java
+++ src/main/java/br/com/bemmatogrosso/domain/model/cardapio/bebidas/Bebida.java
@@ -1,17 +1,30 @@
 package br.com.bemmatogrosso.domain.model.cardapio.bebidas;
 
+import br.com.bemmatogrosso.domain.model.cardapio.vo.Nome;
 import br.com.bemmatogrosso.domain.model.cardapio.vo.Preco;
 import lombok.Getter;
 
 @Getter
 public class Bebida {
 	
-	private String nome;
+	private Nome nome;
 	
 	private Categoria categoria;
 	
 	private Preco preco;
 	
 	private Tipo tipo;
+
+	public Bebida(String nome, Categoria categoria, Preco preco, Tipo tipo) {
+		super();
+		
+		this.nome = new Nome(nome);
+		
+		this.categoria = categoria;
+		this.preco = preco;
+		this.tipo = tipo;
+	}
+	
+	
 	
 }
